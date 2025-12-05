e7b7e4fd268cbb54afd10c1b1f5f2317ff17ed90
Olof Gustavsson <trapsnegel@gmail.com>
2025-12-05 10:48:00 +0100

    day 3 setup

---
+++ src/aoc2025/solutions/day03a.py
@@ -0,0 +1,3 @@
+
+def solve(_input:str) -> int:
+    return None
---
+++ src/tests/test_day03.py
@@ -0,0 +1,33 @@
+import importlib
+
+from aoc2025.inputs import get_sample_inputs, get_challenge_input
+
+DAY_NAME:str = __file__.split('_')[-1].removesuffix('.py')
+
+
+# Part 1 #
+part1_module = importlib.import_module(f'.solutions.{DAY_NAME}a', 'aoc2025')
+
+def test_part1_samples() -> None:
+    samples = get_sample_inputs(DAY_NAME, 'a')
+
+    assert part1_module.solve(samples[0]) == 357
+
+# def test_part1_challenge() -> None:
+#     challenge = get_challenge_input(DAY_NAME)
+
+#     assert part1_module.solve(challenge) == 
+
+
+# Part 2 #
+# part2_module = importlib.import_module(f'.solutions.{DAY_NAME}b', 'aoc2025')
+
+# def test_part2_samples() -> None:
+#     samples = get_sample_inputs(DAY_NAME, 'b')
+
+#     assert part2_module.solve(samples[0]) == 
+
+# def test_part2_challenge() -> None:
+#     challenge = get_challenge_input(DAY_NAME)
+
+#     assert part2_module.solve(challenge) == 
