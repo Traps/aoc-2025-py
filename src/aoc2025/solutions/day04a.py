3b120fc285f0154d3aae3c4c268c87f9118beabc
Olof Gustavsson <trapsnegel@gmail.com>
2025-12-08 11:22:56 +0100

    they're rolls, not scrolls

--- src/aoc2025/solutions/day04a.py
+++ src/aoc2025/solutions/day04a.py
@@ -1,20 +1,20 @@
 from typing import Generator
 
-def iter_scrolls(scroll_map:tuple[str, ...]) -> Generator[tuple[int, int], None, None]:
+def iter_rolls(scroll_map:tuple[str, ...]) -> Generator[tuple[int, int], None, None]:
     for y,row in enumerate(scroll_map):
         yield from ((y,x) for x,c in enumerate(row) if c == '@')
 
 
 def can_move(scroll_map:tuple[str, ...], y:int, x:int) -> bool:
-    n_scrolls = 0
+    n_rolls = 0
     
     for row in scroll_map[max(0, y-1):y+2]:
-        n_scrolls += row[max(0, x-1):x+2].count('@')
+        n_rolls += row[max(0, x-1):x+2].count('@')
 
-    return n_scrolls < 5
+    return n_rolls < 5
 
 
 def solve(_input:str) -> int:
-    scroll_map = tuple(_input.strip().splitlines())
+    rolls = tuple(_input.strip().splitlines())
 
-    return sum(can_move(scroll_map, *pos) for pos in iter_scrolls(scroll_map))
+    return sum(can_move(rolls, *pos) for pos in iter_rolls(rolls))
